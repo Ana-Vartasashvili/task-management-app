037696189d1a1ad803aba9c846f3516bad9aa006
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-02-16 12:05:27 +0400

    give a and b parameters type any in the sort method

--- src/pages/allTasks/AllTasksList.tsx
+++ src/pages/allTasks/AllTasksList.tsx
@@ -4,7 +4,9 @@ import { RootState } from '../../store/store'
 
 const AllTasksList = () => {
   const tasksData = useSelector((state: RootState) => state.tasks.tasks)
-  const tasks = [...tasksData].sort((a, b) => a.completed - b.completed)
+  const tasks = [...tasksData].sort(
+    (a: any, b: any) => a.completed - b.completed
+  )
 
   return (
     <ul className="mt-[3rem] mx-4 flex flex-col gap-3 pb-2">
